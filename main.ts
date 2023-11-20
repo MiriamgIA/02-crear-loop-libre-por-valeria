a1063affd9ed8f1749bd939c5c147ec1cd880ed5
Miriam García <60086516+MiriamgIA@users.noreply.github.com>
2023-11-20 06:52:46 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.showNumber(7)
+    basic.pause(500)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(500)
+    basic.showNumber(8)
+})
